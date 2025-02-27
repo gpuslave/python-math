5a2bebacd40c0cc84593c5699cb2061b28ee13e7
lnemt <alenemt@yandex.ru>
2025-02-27 21:04:39 +0300

    start developing bidirectional logic

--- runge/current.py
+++ runge/current.py
@@ -80,12 +80,14 @@ def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, toler
     y_values : список соответствующих значений y
     """
 
-    x_current = x_start
+    x_current = C
     y_current = y0
     h = h_initial
     h_min_flag = False
 
-    x_values = [x_start]
+    direction = 1 if C == x_start else -1
+
+    x_values = [x_current]
     y_values = [y0]
 
     with open("output.txt", "a", encoding="utf-8") as output_file:
@@ -95,7 +97,9 @@ def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, toler
             f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.5f}\n"
         )
 
-        while x_current < x_end:
+        while ((x_current < x_end) and direction) or (
+            not direction and (x_current > x_start)
+        ):
             if x_current + h > x_end:
                 h = x_end - x_current  # Чтобы точно достичь x_end
 
@@ -142,6 +146,54 @@ def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, toler
                     output_file.write(
                         f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.8f}\t\test:{error_estimation:.8f}\t\tdiff:{(error_estimation - tolerance):.8f}\n"
                     )
+
+        # while x_current < x_end:
+        #     if x_current + h > x_end:
+        #         h = x_end - x_current  # Чтобы точно достичь x_end
+
+        #     error_estimation = tolerance + 1  # Инициализация для входа в цикл while
+
+        #     while error_estimation > tolerance and not h_min_flag:
+        #         if h < h_min:
+        #             output_file.write(
+        #                 f"Предупреждение: Размер шага стал очень маленьким ({h}). Точность может быть не достигнута."
+        #             )
+        #             h_min_flag = True
+        #             break  # Выход из внутреннего цикла, если шаг слишком мал
+
+        #         # Вычисление решения с шагом h, используя 2-й порядок
+        #         y_rk2_step = runge_kutta_2nd_order(f, y_current, x_current, h)
+
+        #         # Вычисление решения с шагом h, используя 3-й порядок (для оценки ошибки)
+        #         y_rk3_step = runge_kutta_3rd_order(f, y_current, x_current, h)
+
+        #         # Оценка ошибки как разница между 2-м и 4-м порядками
+        #         error_estimation = np.abs(y_rk2_step - y_rk3_step)
+
+        #         if error_estimation > tolerance:
+        #             h *= 0.5  # Уменьшение шага вдвое
+        #         else:
+        #             break  # Шаг приемлем
+
+        #     if error_estimation <= tolerance:
+        #         y_current = y_rk2_step  # Принимаем решение 2-го порядка
+        #         x_current += h
+
+        #         output_file.write(
+        #             f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.8f}\t\test:{error_estimation:.8f}\t\tdiff:{(error_estimation - tolerance):.8f}\n"
+        #         )
+
+        #         if h * 2 <= (x_end - x_current) and h * 2 <= h_initial * 2:
+        #             h *= 2.0  # Увеличение шага вдвое для следующего шага, если это возможно и не превышает начальный шаг удвоенный.
+
+        #     else:
+        #         if h <= h_min and x_current < x_end:
+        #             y_current = runge_kutta_2nd_order(f, y_current, x_current, h)
+        #             x_current += h
+
+        #             output_file.write(
+        #                 f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.8f}\t\test:{error_estimation:.8f}\t\tdiff:{(error_estimation - tolerance):.8f}\n"
+        #             )
 
     return x_values, y_values
 
@@ -199,8 +251,8 @@ def visualize_results():
     ax1.grid(True)
 
     # Step size plot (log scale for better visualization)
-    ax2.plot(x_vals, h_vals, "r-", linewidth=1.5)
-    ax2.set_ylim((min(h_vals) * 0.001, max(h_vals) * 0.3))
+    ax2.plot(x_vals[1:], h_vals[1:], "r-", linewidth=1.5)
+    # ax2.set_ylim((min(h_vals) * 0.001, max(h_vals) * 0.3))
     # y_ticks = np.linspace(min(h_vals) * 0.9, max(h_vals) * 1.1, 10)
     # y_ticks = np.logspace(np.log10(min(h_vals) * 0.9), np.log10(max(h_vals) * 1.1), 10)
     # ax2.set_yticks(y_ticks)
@@ -223,8 +275,11 @@ if __name__ == "__main__":
     input_filename = "input.txt"
 
     # Пример использования: dy/dx = y - x^2 + 1, y(0) = 0.5
+    # def f_example(x, y):
+    #     return y - x**2 + 1
+
     def f_example(x, y):
-        return y - x**2 + 1
+        return 2 * x
 
     x_start_example, x_end_example, C, y0_example, h_min, tolerance_example = (
         read_vars_from_file(input_filename)
