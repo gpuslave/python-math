58d22beb156dbec6267d398d8af6d4b46aa00613
alex <shimanoee@yandex.ru>
2025-03-15 12:36:48 +0300

    done runge-kutta algo

--- runge/current.py
+++ runge/current.py
@@ -1,66 +1,60 @@
 import numpy as np
 
+def runge_kutta(f, x_current, y_current, h, direction=1):
+  """
+  returns:
+  (rk2, rk3)
+  """
+  h = direction * h
 
-def runge_kutta_2nd_order(f, y_current, x_current, h):
-    """
-    Решает один шаг задачи Коши методом Рунге-Кутта второго порядка.
+  k1 = h * f(x_current, y_current)
+  k2 = h * f(x_current + h / 2, y_current + k1 / 2)
+  k3 = h * f(x_current + h, y_current - k1 + k2 * 2)
 
-    Аргументы:
-    f : функция, представляющая dy/dx = f(x, y)
-    y_current : текущее значение y
-    x_current : текущее значение x
-    h : размер шага
+  rk2 = y_current + k2
+  rk3 = y_current + (k1 + k2 * 4 + k3) / 6
+  return rk2, rk3
+   
 
-    Возвращает:
-    y_next : следующее значение y
-    """
-    k1 = h * f(x_current, y_current)
-    k2 = h * f(x_current + h / 2, y_current + k1 / 2)
-    y_next = y_current + k2
-    return y_next
+# def runge_kutta_3rd_order(f, y_current, x_current, h):
+#     """
+#     Решает один шаг задачи Коши методом Рунге-Кутта третьего порядка.
 
+#     Аргументы:
+#     f : функция, представляющая dy/dx = f(x, y)
+#     y_current : текущее значение y
+#     x_current : текущее значение x
+#     h : размер шага
 
-def runge_kutta_3rd_order(f, y_current, x_current, h):
-    """
-    Решает один шаг задачи Коши методом Рунге-Кутта третьего порядка.
+#     Возвращает:
+#     y_next : следующее значение y
+#     """
 
-    Аргументы:
-    f : функция, представляющая dy/dx = f(x, y)
-    y_current : текущее значение y
-    x_current : текущее значение x
-    h : размер шага
+#     k1 = h * f(x_current, y_current)
+#     k2 = h * f(x_current + h / 2, y_current + k1 / 2)
+#     k3 = h * f(x_current + h, y_current - k1 + 2 * k2)
+#     y_next = y_current + (k1 + 4 * k2 + k3) / 6
 
-    Возвращает:
-    y_next : следующее значение y
-    """
+#     return y_next
 
-    k1 = h * f(x_current, y_current)
-    k2 = h * f(x_current + h / 2, y_current + k1 / 2)
-    k3 = h * f(x_current + h, y_current - k1 + 2 * k2)
-    y_next = y_current + (k1 + 4 * k2 + k3) / 6
+# def runge_kutta_2nd_order(f, y_current, x_current, h):
+#     """
+#     Решает один шаг задачи Коши методом Рунге-Кутта второго порядка.
 
-    return y_next
+#     Аргументы:
+#     f : функция, представляющая dy/dx = f(x, y)
+#     y_current : текущее значение y
+#     x_current : текущее значение x
+#     h : размер шага
 
+#     Возвращает:
+#     y_next : следующее значение y
+#     """
+#     k1 = h * f(x_current, y_current)
+#     k2 = h * f(x_current + h / 2, y_current + k1 / 2)
+#     y_next = y_current + k2
+#     return y_next
 
-def runge_kutta_4th_order(f, y_current, x_current, h):
-    """
-    Решает один шаг задачи Коши методом Рунге-Кутта четвертого порядка.
-
-    Аргументы:
-    f : функция, представляющая dy/dx = f(x, y)
-    y_current : текущее значение y
-    x_current : текущее значение x
-    h : размер шага
-
-    Возвращает:
-    y_next : следующее значение y
-    """
-    k1 = h * f(x_current, y_current)
-    k2 = h * f(x_current + h / 2, y_current + k1 / 2)
-    k3 = h * f(x_current + h / 2, y_current + k2 / 2)
-    k4 = h * f(x_current + h, y_current + k3)
-    y_next = y_current + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-    return y_next
 
 
 def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, tolerance):
@@ -82,6 +76,10 @@ def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, toler
     h_min_flag = False
 
     direction = 1 if C == x_start else -1
+    if direction == -1:
+      x_start, x_end = x_end, x_start 
+
+    print(x_current, direction, x_start, x_end)
 
     with open("output.txt", "a", encoding="utf-8") as output_file:
         output_file.truncate(0)
@@ -90,11 +88,10 @@ def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, toler
             f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.5f}\n"
         )
 
-        while ((x_current < x_end) and direction) or (
-            not direction and (x_current > x_start)
-        ):
-            if x_current + h > x_end:
-                h = x_end - x_current  # Чтобы точно достичь x_end
+        while x_current * direction < x_end * direction:
+
+            if direction * (x_current + h * direction) > x_end * direction:
+                h = np.abs(x_end - x_current)  # Чтобы точно достичь x_end
 
             error_estimation = tolerance + 1  # Инициализация для входа в цикл while
 
@@ -106,13 +103,9 @@ def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, toler
                     h_min_flag = True
                     break  # Выход из внутреннего цикла, если шаг слишком мал
 
-                # Вычисление решения с шагом h, используя 2-й порядок
-                y_rk2_step = runge_kutta_2nd_order(f, y_current, x_current, h)
+                y_rk2_step, y_rk3_step = runge_kutta(f, x_current, y_current, h, direction)
 
-                # Вычисление решения с шагом h, используя 3-й порядок (для оценки ошибки)
-                y_rk3_step = runge_kutta_3rd_order(f, y_current, x_current, h)
-
-                # Оценка ошибки как разница между 2-м и 4-м порядками
+                # Оценка ошибки как разница между 2-м и 3-м порядками
                 error_estimation = np.abs(y_rk2_step - y_rk3_step)
 
                 if error_estimation > tolerance:
@@ -122,72 +115,23 @@ def solve_cauchy_adaptive_step(f, y0, C, x_start, x_end, h_initial, h_min, toler
 
             if error_estimation <= tolerance:
                 y_current = y_rk2_step  # Принимаем решение 2-го порядка
-                x_current += h
+                x_current += direction * h
 
                 output_file.write(
                     f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.8f}\t\test:{error_estimation:.8f}\t\tdiff:{(error_estimation - tolerance):.8f}\n"
                 )
 
-                if h * 2 <= (x_end - x_current) and h * 2 <= h_initial * 2:
+                if h * 2 <= np.abs(x_end - x_current) and h * 2 <= h_initial * 2:
                     h *= 2.0  # Увеличение шага вдвое для следующего шага, если это возможно и не превышает начальный шаг удвоенный.
 
             else:
-                if h <= h_min and x_current < x_end:
-                    y_current = runge_kutta_2nd_order(f, y_current, x_current, h)
-                    x_current += h
+                if h <= h_min and x_current * direction < x_end * direction:
+                    y_current, _ = runge_kutta(f, x_current, y_current, h)
+                    x_current += direction * h
 
                     output_file.write(
                         f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.8f}\t\test:{error_estimation:.8f}\t\tdiff:{(error_estimation - tolerance):.8f}\n"
                     )
-
-        # while x_current < x_end:
-        #     if x_current + h > x_end:
-        #         h = x_end - x_current  # Чтобы точно достичь x_end
-
-        #     error_estimation = tolerance + 1  # Инициализация для входа в цикл while
-
-        #     while error_estimation > tolerance and not h_min_flag:
-        #         if h < h_min:
-        #             output_file.write(
-        #                 f"Предупреждение: Размер шага стал очень маленьким ({h}). Точность может быть не достигнута."
-        #             )
-        #             h_min_flag = True
-        #             break  # Выход из внутреннего цикла, если шаг слишком мал
-
-        #         # Вычисление решения с шагом h, используя 2-й порядок
-        #         y_rk2_step = runge_kutta_2nd_order(f, y_current, x_current, h)
-
-        #         # Вычисление решения с шагом h, используя 3-й порядок (для оценки ошибки)
-        #         y_rk3_step = runge_kutta_3rd_order(f, y_current, x_current, h)
-
-        #         # Оценка ошибки как разница между 2-м и 4-м порядками
-        #         error_estimation = np.abs(y_rk2_step - y_rk3_step)
-
-        #         if error_estimation > tolerance:
-        #             h *= 0.5  # Уменьшение шага вдвое
-        #         else:
-        #             break  # Шаг приемлем
-
-        #     if error_estimation <= tolerance:
-        #         y_current = y_rk2_step  # Принимаем решение 2-го порядка
-        #         x_current += h
-
-        #         output_file.write(
-        #             f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.8f}\t\test:{error_estimation:.8f}\t\tdiff:{(error_estimation - tolerance):.8f}\n"
-        #         )
-
-        #         if h * 2 <= (x_end - x_current) and h * 2 <= h_initial * 2:
-        #             h *= 2.0  # Увеличение шага вдвое для следующего шага, если это возможно и не превышает начальный шаг удвоенный.
-
-        #     else:
-        #         if h <= h_min and x_current < x_end:
-        #             y_current = runge_kutta_2nd_order(f, y_current, x_current, h)
-        #             x_current += h
-
-        #             output_file.write(
-        #                 f"x:{x_current:.5f}\t\ty:{y_current:.5f}\t\th:{h:.5f}\t\te:{tolerance:.8f}\t\test:{error_estimation:.8f}\t\tdiff:{(error_estimation - tolerance):.8f}\n"
-        #             )
-
 
 def read_vars_from_file(filename):
     file = open(filename, mode="r")
@@ -259,18 +203,19 @@ def visualize_results():
 
     plt.tight_layout()
     plt.savefig("runge_kutta_solution.png", dpi=300)
-    plt.show()
+    plt.close()
 
 
 if __name__ == "__main__":
-    input_filename = "input.txt"
 
     # Пример использования: dy/dx = y - x^2 + 1, y(0) = 0.5
-    # def f_example(x, y):
-    #     return y - x**2 + 1
-
     def f_example(x, y):
-        return 2 * x
+        return y - x**2 + 1
+    input_filename = "input.txt"
+
+    # def f_example(x, y):
+    #     return 2 * x
+    # input_filename = "input2.txt"
 
     x_start_example, x_end_example, C, y0_example, h_min, tolerance_example = (
         read_vars_from_file(input_filename)
